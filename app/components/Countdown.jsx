2fcaf3b1c305b411245fb1e9da4654308ebab6cf
lutay.d <dmytro.lutai@roots.com.ua>
2017-01-05 19:04:47 +0200

    worked with Countdown functionalities

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,18 +4,37 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
     getDefaultProps: function () {
-      return {
-          seconds: 0
-      }
-    },
-    getInitialState: function(){
         return {
             seconds: 0
         }
     },
+    getInitialState: function () {
+        return {
+            seconds: 0,
+            countdownStatus: 'stopped'
+        }
+    },
+    startTimer: function () {
+        this.timer = setInterval(() => {
+            var newSeconds = this.state.seconds - 1;
+            this.setState({
+               seconds: newSeconds >= 0 ? newSeconds : 0
+            });
+        }, 1000);
+    },
+    componentDidUpdate: function (prevProps, prevState) {
+        if (this.state.countdownStatus !== prevState.countdownStatus) {
+            switch (this.state.countdownStatus) {
+                case 'started':
+                    this.startTimer();
+                    break;
+            }
+        }
+    },
     handleSetCountdown: function (seconds) {
         this.setState({
-           seconds: seconds
+            seconds: seconds,
+            countdownStatus: 'started'
         });
     },
     render: function () {
